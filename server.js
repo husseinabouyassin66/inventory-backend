514d57983ab808d7935654b96596a1799f37c0fb
Husseinabouyassin66 <husseinabouyassin66@gmail.com>
2026-05-24 22:44:15 +0300

    fix deployment port binding

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', (req, res) => {
     res.send('Inventory API is running live!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
